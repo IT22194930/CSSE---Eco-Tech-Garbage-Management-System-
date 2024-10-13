4aa2956b0cb879c33fada811e2f8da87ed56f60d
HeshanChamika <it22194930@my.sliit.lk>
2024-10-13 19:23:16 +0530

    Refactor User model to include latitude and longitude fields for storing user's location

--- backend/models/User/User.js
+++ backend/models/User/User.js
@@ -1,3 +1,21 @@
+// const mongoose = require('mongoose');
+
+// const userSchema = new mongoose.Schema({
+//     name: { type: String, required: true },
+//     email: { type: String, required: true, unique: true },
+//     role: { type: String, enum: ['admin', 'user'], default: 'user' }, // Adjust roles as needed
+//     address: { type: String },
+//     phone: { type: String },
+//     photoUrl: { type: String },
+//     qrCodeUrl: { type: String },
+//     userType: { type: String },
+//     balance: { type: Number, default: 0.0 }
+// }, { timestamps: true });
+
+// const User = mongoose.model('User', userSchema);
+
+// module.exports = User;
+
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
@@ -9,7 +27,9 @@ const userSchema = new mongoose.Schema({
     photoUrl: { type: String },
     qrCodeUrl: { type: String },
     userType: { type: String },
-    balance: { type: Number, default: 0.0 }
+    balance: { type: Number, default: 0.0 },
+    latitude: { type: Number },  // Add latitude for storing user's location
+    longitude: { type: Number }, // Add longitude for storing user's location
 }, { timestamps: true });
 
 const User = mongoose.model('User', userSchema);
