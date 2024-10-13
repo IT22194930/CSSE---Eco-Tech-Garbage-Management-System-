98a4e47abeeb4702ddbc2182914e00ee0ba4168d
Nadil Senruka <it22144430@my.sliit.lk>
2024-10-13 21:56:56 +0530

    update payment backend
    update payment backend

--- backend/models/User/TransactionDetail.js
+++ backend/models/User/TransactionDetail.js
@@ -1,12 +1,17 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const transactionSchema = new mongoose.Schema({
-    userId: { type: String, required: true },
-    amount: { type: Number, default: 0.0 },
-    transactionType: { type: String },
-    date: { type: Date, default: Date.now },
+  userId: { type: String, required: true },
+  amount: { type: Number, default: 0.0 },
+  cashback: { type: Number, defult: 0.0 },
+  specialCost: { type: Number, defult: 0.0 },
+  transactionType: { type: String },
+  date: { type: Date, default: Date.now },
 });
 
-const TransactionDetail = mongoose.model('TransactionDetail', transactionSchema);
+const TransactionDetail = mongoose.model(
+  "TransactionDetail",
+  transactionSchema
+);
 
 module.exports = TransactionDetail;
